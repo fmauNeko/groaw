47c6a1261a6eee548eecfc84140c0613f3ce73c7
fmauNeko <mail@fmauneko.eu>
2010-08-14 22:12:21 +0200

    Ajout d'une fonction pour convertir les en-têtes MIME en chaînes UTF-8

--- Commun/View/AVueModele.php
+++ Commun/View/AVueModele.php
@@ -8,5 +8,16 @@ abstract class AVueModele
 	{
 		$this->modele = $modele;
 	}
+
+        public function mime_to_utf8($input)
+        {
+                $output = "";
+                $elements = imap_mime_header_decode($text);
+                for ($i=0; $i<count($elements); $i++) {
+                        $output .= iconv($elements[$i]->charset, "UTF-8", $elements[$i]->text);
+                }
+
+                return $output;
+        }
 }
 ?>
