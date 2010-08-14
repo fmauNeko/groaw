7ab68d634b2df6a7875a9db405ef0eaa6461b933
Antoine <antoine@blogjaune.fr>
2010-08-14 22:46:02 +0200

    plus d'informations pour les gens heureux

--- View/CVueListe.php
+++ View/CVueListe.php
@@ -9,9 +9,13 @@ class CVueListe extends AVueModele
 		foreach ($this->modele->messages as $message)
 		{
 			echo "\t<tr><td>",
-				 $message->msgno,
-				 "</td><td>",
-				 htmlspecialchars($this->mime_to_utf8($message->subject)),
+				$message->msgno,
+				"</td><td>",
+				htmlspecialchars($this->mime_to_utf8($message->subject)),
+				"</td><td>",
+				htmlspecialchars($this->mime_to_utf8($message->from)),
+				"</td><td>",
+				htmlspecialchars($message->date),
 				"</td></tr>\n";
 		}
 
