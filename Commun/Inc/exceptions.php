8a63017e4625c256389f1190606009089a6e30d4
Antoine <antoine@blogjaune.fr>
2010-08-14 15:20:35 +0200

    c'est plus gentil tout ça

--- Commun/Inc/exceptions.php
+++ Commun/Inc/exceptions.php
@@ -1,17 +1,15 @@
 <?php
-$lapin = true;
 // Transformation des messages d'erreurs en exceptions
+$exception_error_handler_semaphore = true;
 function exception_error_handler($errno, $errstr, $errfile, $errline)
 {
-	global $lapin;
-	var_dump($errno);
-	var_dump($errstr);
-	var_dump($errfile);
-	var_dump($errline);
-	var_dump($lapin);
-	if ($lapin)
+	// L'utilité de ce truc est de contourner un bug de merde de php
+	// Quand une fonction créé deux erreurs,
+	// la deuxième exception est attaquée avant que la première soit relachée
+	global $exception_error_handler_semaphore;
+	if ($exception_error_handler_semaphore)
 	{
-		$lapin = false;
+		$exception_error_handler_semaphore = false;
 		throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
 	}
 }
@@ -21,6 +19,7 @@ set_error_handler("exception_error_handler");
 function exception_handler($exception)
 {
 	echo "\"<div class =\"exception\" style=\"width:80%;color:red;border:2px solid orange;white-space:pre;\">\n", htmlspecialchars($exception), "\n<br/><a href=\"javascript:history.back()\">Revenir en arrière</a></div>\"";
+	die("exception méchante");
 }
 set_exception_handler("exception_handler");
 
