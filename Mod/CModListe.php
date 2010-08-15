2079a1c5effeb70655a62ee0eb1d61eb7f2f6d1e
Antoine <antoine@blogjaune.fr>
2010-08-15 12:23:42 +0200

    bug lorsqu'il n'y a pas de messages dans la liste

--- Mod/CModListe.php
+++ Mod/CModListe.php
@@ -9,17 +9,24 @@ class CModListe extends AModele
 		$liste_triee = CImap::sort(SORTDATE, 1);
 		$nb_entetes = count($liste_triee);
 
-		$liste_entetes = CImap::fetch_overview("1:$nb_entetes");
-
-		$liste_finale = $liste_triee;
-
-		foreach ($liste_entetes as $entete)
+		if ($nb_entetes === 0)
 		{
-			$clef = array_search($entete->msgno,$liste_triee);
-			$liste_finale[$clef] = $entete;
+			$this->messages = array();
 		}
+		else
+		{
+			$liste_entetes = CImap::fetch_overview("1:$nb_entetes");
 
-		$this->messages = $liste_finale;
+			$liste_finale = $liste_triee;
+
+			foreach ($liste_entetes as $entete)
+			{
+				$clef = array_search($entete->msgno,$liste_triee);
+				$liste_finale[$clef] = $entete;
+			}
+
+			$this->messages = $liste_finale;
+		}
 	}
 
 	public function recupererBoites()
