1fee2e4fc8a8ba378526c7045c586f79fbdea59d
Antoine <antoine@blogjaune.fr>
2010-09-30 16:38:05 +0200

    mode raw

--- Ctrl/Courriels.php
+++ Ctrl/Courriels.php
@@ -2,16 +2,24 @@
 $NOM_CTRL = 'Courriels';
 
 $ACTIONS = array(
-	'afficher'		=> array('Afficher','Afficher un courriel'),
-	'liste' => array('Messages','Liste des mails')
+	'afficher'	=> array('Afficher','Afficher un courriel'),
+	'raw'		=> array('Afficher en raw','Afficher un courriel sans transformations'),
+	'liste'		=> array('Messages','Liste des mails')
 );
 
-$DEFAULT_ACTION = 'afficher';
+$DEFAULT_ACTION = 'liste';
 
 require ('../Inc/haut.php');
 // Début de la liste des fonctions
 
 function afficher()
+{
+	$numero =  isset($_REQUEST['numero']) ? intval($_REQUEST['numero']) : 1;
+
+	echo nl2br(htmlspecialchars(CImap::body($numero)));
+}
+
+function raw()
 {
 	$numero =  isset($_REQUEST['numero']) ? intval($_REQUEST['numero']) : 1;
 
